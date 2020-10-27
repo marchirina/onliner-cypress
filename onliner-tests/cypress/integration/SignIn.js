d822b17c3189ceffb645f596dce25cb209087ef7
Irina March <marchankairyna@gmail.com>
2020-10-27 21:44:58 +0300

    test

--- onliner-tests/cypress/integration/SignIn.js
+++ onliner-tests/cypress/integration/SignIn.js
@@ -1,12 +1,12 @@
 /// <reference types = "cypress" />
 
-describe('Test log in', () => {
+describe('Test login', () => {
 
     beforeEach('login to the app', () => {
         cy.loginToApplication()
     })
 
-    it('verify user can log in successfully', () => {
+    it('verify user can login successfully', () => {
         cy.get('.b-top-profile__preview').find('div').should('have.class','js-header-user-avatar')
     })
 })
